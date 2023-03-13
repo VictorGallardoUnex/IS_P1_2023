24f0a68eca52cdada7070f2b8eafe04e5ded3f29
ThePromidius <thepromidiusyt@gmail.com>
2023-03-13 11:19:40 +0100

    Formateado multilinea

--- src/Utils.java
+++ src/Utils.java
@@ -46,45 +46,42 @@ public class Utils {
      * Muestra la ayuda
      */
     public static void mostrar_ayuda(){
-        syso.println("""
-                Sintaxis
-                    IsP1 -fe <fichero1> [-fs <fichero2>]| -h
-                    -h Muestra este mensaje de ayuda
-                    -fe El fichero que contiene la configuracion/comandos a ejecutar
-                    -fs El fichero de salida donde guardar la salida. "salida.txt" por defecto
-                
-                Banderas:
-                o @ salidapantalla ON | OFF
-                    Indica si se muestra por la terminal la salida del programa
-                o @ salidafichero ON | OFF
-                    Indica si se muestra en el archivo de texto la salida del programa
-                o @ reescribirficherosalida
-                    Indica si se sobreescribe el archivo de salida por defecto o no
-                Comandos
-                o & ficheroSalida <nombre>
-                    En <nombre> vendrá una cadena para indicar cual será el fichero
-                    de salida que se usará a partir de ese instante. En el caso de que la
-                    bandera de salidafichero esté en ON se procederá a ponerla en
-                    OFF; y se ejecutará todo el código asociado a la aparición de esta
-                    bandera en OFF en el fichero de configuración
-                o & seleccionatarjeta [<numero>]
-                    Elegirá en el sistema el numero de tarjeta asociado para trabajar
-                    con ella en el resto de comandos si procede
-                    Si no existe esa tarjeta en el sistema no se cambiará el numero de
-                    tarjeta de trabajo, y además informará de que no es posible el
-                    cambio
-                o & infotarjeta [<numero>]
-                    Mostrará la información completa de la tarjeta seleccionada en el
-                    <numero>. Si no se indica el número, se mostrará la de la tarjeta
-                    elegida,.
-                o & infoIP [<numero>]
-                    Mostrará la información sobre la dirección IP de la tarjeta asociada
-                    a <numero>
-                o & infoEthernet [<numero>]
-                    Mostrará la información sobre la dirección Ethernet de la tarjeta
-                    asociada a <numero>
-                """
-        );
+        syso.println("    Sintaxis");
+        syso.println("        IsP1 -fe <fichero1> [-fs <fichero2>]| -h");
+        syso.println("        -h Muestra este mensaje de ayuda");
+        syso.println("        -fe El fichero que contiene la configuracion/comandos a ejecutar");
+        syso.println("        -fs El fichero de salida donde guardar la salida. 'salida.txt' por defecto");
+        syso.println("        ");
+        syso.println("    Banderas:");
+        syso.println("        o @ salidapantalla ON | OFF");
+        syso.println("            Indica si se muestra por la terminal la salida del programa");
+        syso.println("        o @ salidafichero ON | OFF");
+        syso.println("            Indica si se muestra en el archivo de texto la salida del programa");
+        syso.println("        o @ reescribirficherosalida");
+        syso.println("            Indica si se sobreescribe el archivo de salida por defecto o no");
+        syso.println("    Comandos");
+        syso.println("        o & ficheroSalida <nombre>");
+        syso.println("            En <nombre> vendrá una cadena para indicar cual será el fichero");
+        syso.println("            de salida que se usará a partir de ese instante. En el caso de que la");
+        syso.println("            bandera de salidafichero esté en ON se procederá a ponerla en");
+        syso.println("            OFF; y se ejecutará todo el código asociado a la aparición de esta");
+        syso.println("            bandera en OFF en el fichero de configuración");
+        syso.println("        o & seleccionatarjeta [<numero>]");
+        syso.println("            Elegirá en el sistema el numero de tarjeta asociado para trabajar");
+        syso.println("            con ella en el resto de comandos si procede");
+        syso.println("            Si no existe esa tarjeta en el sistema no se cambiará el numero de");
+        syso.println("            tarjeta de trabajo, y además informará de que no es posible el");
+        syso.println("            cambio");
+        syso.println("        o & infotarjeta [<numero>]");
+        syso.println("            Mostrará la información completa de la tarjeta seleccionada en el");
+        syso.println("            <numero>. Si no se indica el número, se mostrará la de la tarjeta");
+        syso.println("            elegida,.");
+        syso.println("        o & infoIP [<numero>]");
+        syso.println("            Mostrará la información sobre la dirección IP de la tarjeta asociada");
+        syso.println("            a <numero>");
+        syso.println("        o & infoEthernet [<numero>]");
+        syso.println("            Mostrará la información sobre la dirección Ethernet de la tarjeta");
+        syso.println("            asociada a <numero>");
     }
 
 
